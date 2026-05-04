fcb7b4f44d690b67784c069ada4d1150adc739da
Константин С. Васильев <K118@sa.local>
2026-05-04 10:25:19 +0700

    1

--- auth/auth.py
+++ auth/auth.py
@@ -43,7 +43,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
